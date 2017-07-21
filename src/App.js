b76303a610c7e3a41c52dcf0576f74ee2d38f60a
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-07-20 22:46:27 -0400

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 const PLACES = [
